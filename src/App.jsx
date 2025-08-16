9ddc72c1664912a61b211928e87622725b02f8fc
Priyanshu samal <samalpriyanshu966@gmail.com>
2025-08-16 16:56:11 +0530

    dusra

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,20 @@ import './App.scss'
 import Header from './components/Header'
 import Profile from './components/Profile'
 import { FloatingDock } from './components/FloatingDock'
+import NameDisplay from './components/NameDisplay'
 
 const App = () => {
+
+
   return (
     <div className="app">
       <div className="container">
         <div className="hero-section">
           <Header />
           <Profile />
+          <NameDisplay />
         </div>
+        
         <FloatingDock/>
       </div>
     </div>
---
+++ src/components/NameDisplay.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './NameDisplay.scss';
+
+const NameDisplay = () => {
+  return (
+    <div className="name-display">
+      <p className="greeting">Hi, I'm</p>
+      <h1 className="name">Priyanshu Samal</h1>
+    </div>
+  );
+};
+
+export default NameDisplay;
