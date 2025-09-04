145df2dbeaf98d1434086197fe5b1efbd93eb425
Priyanshu Samal <samalpriyanshu966@gmail.com>
2025-09-04 16:13:24 +0530

    Update ThemeContext.jsx

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -5,10 +5,10 @@ const ThemeContext = createContext();
 export const useTheme = () => useContext(ThemeContext);
 
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState('light');
+  const [theme, setTheme] = useState('dark');
 
   const toggleTheme = () => {
-    setTheme((prevTheme) => (prevTheme === 'dark' ? 'light' : 'dark'));
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
   };
 
   return (
